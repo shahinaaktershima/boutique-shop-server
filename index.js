47ee9c407d8e2a51ed551ef44f64596de1798893
shima <shima421914@gmail.com>
2024-02-08 15:54:47 +0600

    admin-dashboard

--- index.js
+++ index.js
@@ -38,25 +38,23 @@ async function run() {
 
   
   // users related api
-  app.get('/user/admin/:email',async(req,res)=>{
-  const email=req.params.email;
-  if(email!== req.decoded.email){
-  return res.status(403).send({message:'forbidden access'})
-  }
-  const query={email:email};
-  const user=await usersCollection.findOne(query);
-  let admin=false;
-  if(user){
-    admin =user?.role ==='admin';
+  // app.get('/user/admin/:email',async(req,res)=>{
+  // const email=req.params.email;
+  // if(email!== req.decoded.email){
+  // return res.status(403).send({message:'forbidden access'})
+  // }
+  // const query={email:email};
+  // const user=await usersCollection.findOne(query);
+  // let admin=false;
+  // if(user){
+  //   admin =user?.role ==='admin';
   
-  }
-  res.send({admin})
-  })
+  // }
+  // res.send({admin})
+  // })
   app.post('/user',async(req,res)=>{
   
     const user=req.body;
-    // insert email if user doesn't exist :
-    // you can do this many ways (1.email unique,2. upsert  3.simple checking)
     const query={email:user.email}
     const existingUser=await usersCollection.findOne(query);
     if(existingUser){
@@ -70,8 +68,35 @@ async function run() {
     const result=await usersCollection.find().toArray();
     res.send(result);
   });
-
-
+// for admin
+app.patch('/user/admin/:id',async(req,res)=>{
+  const id=req.params.id;
+  const filter={_id: new ObjectId(id)}
+  const updateDoc = {
+    $set: {
+      role:'admin'
+    },
+  };
+  const result=await usersCollection.updateOne(filter,updateDoc);
+  res.send(result)
+})
+app.get('/user/admin/:email',async(req,res)=>{
+  const email=req.params.email;
+  console.log(email);
+  const query={email:email};
+  const user=await usersCollection.findOne(query);
+  let admin=false;
+  if(user){
+    admin =user?.role ==='admin';
+  }
+  res.send({admin})
+  })
+app.delete('/user/:id',async(req,res)=>{
+  const id=req.params.id;
+  const query={_id: new ObjectId(id)}
+  const result=await usersCollection.deleteOne(query);
+  res.send(result)
+});
 
 const trans_id=new ObjectId().toString();
    app.post('/deposit',async(req,res)=>{
